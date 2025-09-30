66932d4e2ec0a731c3a0f5d5f9d4bd5d3a88c6f7
aminelli <antonio.minelli@etlforma.com>
2025-09-30 14:32:52 +0200

    upd

--- ESERCIZI/DAY-002/ES004/demo/src/main/java/com/example/demo/IndexController.java
+++ ESERCIZI/DAY-002/ES004/demo/src/main/java/com/example/demo/IndexController.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-RestController
+@RestController
 public class IndexController {
 
     @RequestMapping("/")
