69077e0b082caa9c1e9f14f34a214cc7a3c3cca9
aminelli <antonio.minelli@etlforma.com>
2025-09-30 11:10:26 +0200

    upd

--- ESERCIZI/DAY-002/ES002/script-vol-002-volumes.sh
+++ ESERCIZI/DAY-002/ES002/script-vol-002-volumes.sh
@@ -5,4 +5,4 @@ docker volume create vol-test-001
 
 
 docker run -it --name deb-001 -v vol-test-001:/data debian
-
+docker run -it --name deb-002 --mount type=volume,src=vol-test-001,dst=/data debian
