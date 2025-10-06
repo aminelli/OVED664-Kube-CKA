80b46001bb3fe78f5ef98731038c725653b2db2f
aminelli <antonio.minelli@etlforma.com>
2025-10-06 11:57:58 +0200

    upd

--- ESERCIZI/DAY-004/ES001/script-01.sh
+++ ESERCIZI/DAY-004/ES001/script-01.sh
@@ -1,8 +1,8 @@
-#!/nin/bash
+#!/bin/bash
 
 # L'effetto di questo comando è quello di creare un Pod che esegue un container basato sull'immagine echoserver.
 # L'immagine echoserver è un semplice server web che risponde alle richieste HTTP sulla 8080
 # Automaticamente, kubernetes, crea la coppia chiave valore run=echoserver come label sul pod
-kubectl run echoserver --image=k8s.gcr.io/echoserver:1.10 --restart=Never --port=8080
+kubectl run echoserver --image=k8s.gcr.io/echoserver:1.10 --restart=Never --port=8080 -n poste-01
 
 
