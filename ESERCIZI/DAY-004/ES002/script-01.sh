ba4cbd9b8a9106fbf42a6b96487317f83e7a7884
aminelli <antonio.minelli@etlforma.com>
2025-10-06 15:21:10 +0200

    upd

--- ESERCIZI/DAY-004/ES002/script-01.sh
+++ ESERCIZI/DAY-004/ES002/script-01.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
 kubectl apply -f clusterip-deploy.yaml
- 
+
+
+# DNS:
+# test-svc.poste-01.svc.cluster.local
+
+
+# Comandi nel container di prova:
+# apt-get update
+# apt-get install -y iputils-ping net-tools wget nano htop traceroute
+
+# DComandi da Host:
+# kubectl get pods  -n poste-01
+# kubectl describe pod echoserver-pod  -n poste-01
+# kubectl get deployments -n poste-01
+# kubectl get services -n poste-01
+# kubectl get pods -n poste-01
+# kubectl get pod test-deploy-56c6f44488-6cx5g -o wide -n poste-01
+
+
