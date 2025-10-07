e30dc27dbc8b243bce52e8327f0aa1bec528c836
aminelli <antonio.minelli@etlforma.com>
2025-10-07 15:25:25 +0200

    upd

--- ESERCIZI/DAY-005/ES004/script-01.sh
+++ ESERCIZI/DAY-005/ES004/script-01.sh
@@ -3,6 +3,7 @@ echo "========================================================================"
 kubectl apply -f vol-01-effimero.yaml
 kubectl apply -f vol-02-effimero-cache.yaml
 kubectl apply -f vol-03-effimero-configmap.yaml
+kubectl apply -f vol-04-effimero-secret.yaml
 echo "========================================================================"
 echo ""
 
