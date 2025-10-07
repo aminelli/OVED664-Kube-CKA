a2047923b23c34703108c03bd34b3b29e6031313
aminelli <antonio.minelli@etlforma.com>
2025-10-07 15:02:40 +0200

    upd

--- ESERCIZI/DAY-005/ES004/script-01.sh
+++ ESERCIZI/DAY-005/ES004/script-01.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "========================================================================"
 kubectl apply -f vol-01-effimero.yaml
+kubectl apply -f vol-02-effimero-cache.yaml
 echo "========================================================================"
 echo ""
 
